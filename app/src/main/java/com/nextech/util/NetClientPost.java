7e4ba8db09c2bd8998604558b6e35bb55fafbed8
Shahadeo Chavan <shahadeochavan@gmail.com>
2017-01-11 19:49:13 +0530

    addnew file netclientpost

---
+++ app/src/main/java/com/nextech/util/NetClientPost.java
@@ -0,0 +1,57 @@
+package com.nextech.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by welcome on 1/11/2017.
+ */
+public class NetClientPost {
+    public String netClientPost(String requerdUrl,String jsondata) {
+
+        try {
+
+            URL url = new URL(requerdUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json");
+
+            OutputStream os = conn.getOutputStream();
+            os.write(jsondata.getBytes());
+            os.flush();
+
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_CREATED) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+        return  null;
+    }
+
+}
--- app/src/main/java/nextech/com/pspolitics/AskQuestinsFragment.java
+++ app/src/main/java/nextech/com/pspolitics/AskQuestinsFragment.java
@@ -12,12 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.nextech.util.HTTPURLConnection;
+import com.nextech.util.NetClientPost;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import nextech.com.pspolitics.votinglistpojo.AskQuestionPojo;
 
 public class AskQuestinsFragment extends Fragment {
     public AskQuestinsFragment() {
@@ -26,12 +27,14 @@ public class AskQuestinsFragment extends Fragment {
     private ProgressDialog pDialog;
     private JSONObject json;
     private int success = 0;
-    String firstname, lastname, email, mobile;
+    String firstname, lastname, email, mobile, questions;
     private HTTPURLConnection service;
     String MobilePattern = "[0-9]{10}+";
     Button btnReg;
-    EditText edtfirstname, edtLastname, edtmobile, edtemail;
-    private String path = "http://192.168.2.105:8080/PSPolitics/json/askquestins/post";
+    EditText edtfirstname, edtLastname, edtmobile, edtemail, edtquestions;
+    String response;
+    String jsonbody;
+    private String url = "http://192.168.42.174:8080/PSPolitics/json/askquestins/post";
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,35 +47,50 @@ public class AskQuestinsFragment extends Fragment {
         edtLastname = (EditText) v.findViewById(R.id.text_last);
         edtmobile = (EditText) v.findViewById(R.id.text_phone);
         edtemail = (EditText) v.findViewById(R.id.email);
+        edtquestions = (EditText) v.findViewById(R.id.edt_ask);
         btnReg = (Button) v.findViewById(R.id.btn_submit);
         service = new HTTPURLConnection();
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AskQuestionPojo askQuestionPojo = new AskQuestionPojo();
+
                 if (edtfirstname.getText().toString().length() == 0) {
                     Toast.makeText(getApplicationContext(), "First Name cannot be Blank", Toast.LENGTH_LONG).show();
                     edtfirstname.setError("First Name cannot be Blank");
-                    firstname = edtfirstname.getText().toString();
                     return;
 
                 } else if (edtLastname.getText().toString().length() == 0) {
                     Toast.makeText(getApplicationContext(), "Last Name cannot be Blank", Toast.LENGTH_LONG).show();
                     edtLastname.setError("Last Name cannot be Blank");
-                    edtLastname.getText().toString();
+
                     return;
                 } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(edtemail.getText().toString()).matches()) {
                     //Validation for Invalid Email Address
                     Toast.makeText(getApplicationContext(), "Please enter valid Email", Toast.LENGTH_LONG).show();
                     edtemail.setError("Please enter valid Email");
-                    edtemail.getText().toString();
+
                     return;
                 } else if (!edtmobile.getText().toString().matches(MobilePattern)) {
                     Toast.makeText(getApplicationContext(), "Please enter valid 10 digit phone number", Toast.LENGTH_SHORT).show();
                     edtmobile.setError("Please enter valid 10 digit phone number");
-                    edtmobile.getText().toString();
+
                     return;
                 } else {
                     Toast.makeText(getApplicationContext(), "Validated Succesfully", Toast.LENGTH_LONG).show();
+                    firstname = edtfirstname.getText().toString();
+                    askQuestionPojo.setFirst_name(firstname);
+                    lastname = edtLastname.getText().toString();
+                    askQuestionPojo.setLast_name(lastname);
+                    email = edtemail.getText().toString();
+                    askQuestionPojo.setEmail(email);
+                    askQuestionPojo.setPhone_number(edtmobile.getText().toString());
+                    askQuestionPojo.setQuestion(edtquestions.getText().toString());
+                    Gson gson = new Gson();
+                    jsonbody = gson.toJson(askQuestionPojo);
+                    System.out.println("response is" + response);
+                    AsynkAskQuestions asynkAskQuestions = new AsynkAskQuestions();
+                    asynkAskQuestions.execute();
                 }
 
             }
@@ -83,62 +101,38 @@ public class AskQuestinsFragment extends Fragment {
 
 
         });
-        new PostDataTOServer().execute();
         return v;
 
     }
 
-    private class PostDataTOServer extends AsyncTask<Void, Void, Void> {
-
-        String response = "";
-        //Create hashmap Object to send parameters to web service
-        HashMap<String, String> postDataParams;
+    public class AsynkAskQuestions extends AsyncTask<String, String, String> {
+        private ProgressDialog pdLoading = new ProgressDialog(AskQuestinsFragment.this.getContext());
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
 
-            pDialog = new ProgressDialog(AskQuestinsFragment.this.getContext());
-            pDialog.setMessage("Please wait...");
-            pDialog.setCancelable(false);
-            pDialog.show();
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
         }
 
         @Override
-        protected Void doInBackground(Void... arg0) {
-            postDataParams = new HashMap<>();
-            postDataParams.put("firstname", firstname);
-            postDataParams.put("lastname", lastname);
-            postDataParams.put("email", email);
-            postDataParams.put("mobile", mobile);
-            //Call ServerData() method to call webservice and store result in response
-            response = service.ServerData(path, postDataParams);
-            try {
-                json = new JSONObject(response);
-                //Get Values from JSONobject
-                System.out.println("success=" + json.get("success"));
-                success = json.getInt("success");
+        protected String doInBackground(String... params) {
+            NetClientPost netClientPost = new NetClientPost();
+            response = netClientPost.netClientPost(url, jsonbody);
+            System.out.println("Respsone is : " + response);
+            return response;
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return null;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            if (pDialog.isShowing())
-                pDialog.dismiss();
-            if (success == 1) {
-                Toast.makeText(getApplicationContext(), "Your information  Added successfully..!", Toast.LENGTH_LONG).show();
-            }
+        protected void onPostExecute(String result) {
+            JSONObject jsonObject = new JSONObject();
+
         }
 
-        private Context getApplicationContext() {
-            return getContext();
-        }
     }
-
 }
-
